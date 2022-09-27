e431b57dcd8798ca28b4c73e12f74e99229254df
jahcoto <jahcoto@gmail.com>
2022-09-27 11:05:36 -0700

    Add images, html and style to each project in the portfolio section

--- src/components/Portafolio.jsx
+++ src/components/Portafolio.jsx
@@ -6,19 +6,24 @@ const Portafolio = () => {
   return (
     <div className="page">
       <h1 className="heading">Portafolio</h1>
-      {trabajos.map(trabajo => {
-        return (
-          <>
-            <article key={trabajo.id}>
-              <span>{trabajo.Category}</span>
-              <h2>
-                <Link to={`/project/${trabajo.url}`}>{trabajo.name}</Link>
-              </h2>
-              <h3>{trabajo.technologies}</h3>
-            </article>
-          </>
-        );
-      })}
+      <section className="works">
+        {trabajos.map(trabajo => {
+          return (
+            <>
+              <article key={trabajo.id} className="work-item">
+                <div className="mask">
+                  <img src={`/public/images/${trabajo.id}.png`} alt="" />
+                </div>
+                <span>{trabajo.Category}</span>
+                <h2>
+                  <Link to={`/project/${trabajo.url}`}>{trabajo.name}</Link>
+                </h2>
+                <h3>{trabajo.technologies}</h3>
+              </article>
+            </>
+          );
+        })}
+      </section>
     </div>
   );
 };
--- src/data/trabajos.js
+++ src/data/trabajos.js
@@ -17,4 +17,13 @@ export const trabajos = [
     'description':
       'App para ver el clima de una ciudad en especifico que el usuario desee ver. Esta hecha para una lista especifica de paises la cual se puede ampliar.',
   },
+  {
+    'id': 'weather-app3',
+    'name': 'weather-app2',
+    'url': 'https://weather-app-chi-ivory-45.vercel.app',
+    'technologies': 'Reactjs, CSS',
+    'Category': 'ReactJS-APP',
+    'description':
+      'App para ver el clima de una ciudad en especifico que el usuario desee ver. Esta hecha para una lista especifica de paises la cual se puede ampliar.',
+  },
 ];
