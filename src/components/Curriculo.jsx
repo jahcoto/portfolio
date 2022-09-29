d79b08b4ab8b9ac035b66ce3dfff1bfa4f77b76d
jahcoto <jahcoto@gmail.com>
2022-09-29 09:10:58 -0700

    Start with curriculo page, add the first styles

--- src/components/Curriculo.jsx
+++ src/components/Curriculo.jsx
@@ -1,10 +1,21 @@
 const Curriculo = () => {
   return (
     <div className="page">
-      <h1 className="heading">Curriculo</h1>
-      <h2>Experiencia</h2>
-      <h2>Conocimientos</h2>
-      <h2>Educación</h2>
+      <div className="curriculo">
+        <h1 className="heading">Jonathan Hernández Coto</h1>
+        <div className="personal-info">
+          <h2>Información Personal</h2>
+          <div className="residencia">
+            <h3 className="subtitle">Lugar de residencia:</h3>
+            <h4>Costa Rica</h4>
+            <p>País</p>
+          </div>
+        </div>
+
+        <h2>Experiencia</h2>
+        <h2>Conocimientos</h2>
+        <h2>Educación</h2>
+      </div>
     </div>
   );
 };
