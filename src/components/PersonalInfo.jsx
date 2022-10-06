acbff44650835ca3d444d35e22658c28740a7ce1
jahcoto <jahcoto@gmail.com>
2022-10-06 11:23:16 -0600

    Update personal info component

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -5,47 +5,56 @@ const PersonalInfo = () => {
     <div className="container">
       <h2 className="title-text">Información Personal</h2>
       <div className="residencia">
-        <h3 className="subtitle">Lugar de residencia:</h3>
+        <div className="encabezado">
+          <h3>Lugar de residencia:</h3>
+        </div>
         <div className="first">
-          <div className="pais">
-            <h4>Costa Rica</h4>
-            <p>País</p>
+          <div className="first-top">
+            <div className="pais">
+              <p>País:</p>
+              <h4>Costa Rica</h4>
+            </div>
+            <div className="provincia">
+              <p>Provincia:</p>
+              <h4>Cartago</h4>
+            </div>
           </div>
-          <div className="provincia">
-            <h4>Cartago</h4>
-            <p>Provincia</p>
-          </div>
-          <div className="canton">
-            <h4>Cartago</h4>
-            <p>Canton</p>
-          </div>
-          <div className="distrito">
-            <h4>Guadalupe o Arenilla</h4>
-            <p>Distrito</p>
+          <div className="first-bottom">
+            <div className="canton">
+              <p>Canton:</p>
+              <h4>Cartago</h4>
+            </div>
+            <div className="distrito">
+              <p>Distrito:</p>
+              <h4>Guadalupe o Arenilla</h4>
+            </div>
           </div>
         </div>
         <div className="second">
           <div className="direccion">
+            <p>Direccion:</p>
             <h4>
               Del Super la Violetera, 300 metros oeste y 75 metros sur, casa
               color lila a mano izquierda.
             </h4>
-            <p>Direccion</p>
           </div>
         </div>
-        <h3 className="subtitle">Otros datos:</h3>
+        <div className="encabezado">
+          <h3>Otros datos:</h3>
+        </div>
+
         <div className="otros-datos">
           <div className="identificacion">
+            <p>Cedula:</p>
             <h4>3-0394-0911</h4>
-            <p>Cedula</p>
           </div>
           <div className="telefono">
+            <p>#Celular:</p>
             <h4>+506 8503-7325</h4>
-            <p>#Celular</p>
           </div>
           <div className="email">
+            <p>e-mail:</p>
             <h4>jahcoto@gmail.com</h4>
-            <p>e-mail</p>
           </div>
         </div>
       </div>
