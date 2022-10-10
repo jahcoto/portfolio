e4e8fd01a4e82425a422ade4ecb028414473d1ed
jahcoto <jahcoto@gmail.com>
2022-10-10 12:06:47 -0600

    Update trabajos

--- src/data/trabajos.js
+++ src/data/trabajos.js
@@ -10,14 +10,15 @@ export const trabajos = [
       'App para ver el clima de una ciudad en especifico que el usuario desee ver. Esta hecha para una lista especifica de paises la cual se puede ampliar.',
   },
   {
-    'id': 'weather-app2',
-    'name': 'weather-app2',
-    'url': 'weather-app2',
-    'external_url': 'https://weather-app-chi-ivory-45.vercel.app',
+    'id': 'password-generator-app',
+    'name': 'password-generator-app',
+    'url': 'password-generator-app',
+    'external_url':
+      'https://jahcoto-password-generator-apppassword-generator-app.vercel.app',
     'technologies': 'Reactjs, CSS',
     'Category': 'ReactJS-APP',
     'description':
-      'App para ver el clima de una ciudad en especifico que el usuario desee ver. Esta hecha para una lista especifica de paises la cual se puede ampliar.',
+      'App la generacion de passwords con un maximo de 20 caracteres, incluyendo letras mayusculas, minusculas, numeros y simbolos.',
   },
   {
     'id': 'weather-app3',
