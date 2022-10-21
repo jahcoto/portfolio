b4bd134840913dffe89a3b33845abc86f06cf4fb
jahcoto <jahcoto@gmail.com>
2022-10-21 13:21:22 -0600

    Add whatasapp to send message and  form validation in contacto page

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -12,26 +12,31 @@ const Contacto = () => {
 
   const handleSendEmail = e => {
     e.preventDefault();
-    console.log(e.target);
-    emailjs
-      .sendForm(
-        'service_o56d2wt',
-        'template_9dzp6ca',
-        form.current,
-        'k17CIQh_qS8gMklCG',
-      )
-      .then(
-        result => {
-          console.log(result.text);
-          alert('Email enviado correctamente!!!');
-        },
-        error => {
-          console.log(error.text);
-          alert('Error al enviar el email!!!');
-        },
-      );
-
-    service_o56d2wt;
+    if (!nombre) {
+      alert('El campo nombre es requerido!!!');
+    } else if (!reply_to) {
+      alert('El campo email es requerido!!!');
+    } else if (!message) {
+      alert('El campo motivo es requerido!!!');
+    } else {
+      emailjs
+        .sendForm(
+          'service_o56d2wt',
+          'template_9dzp6ca',
+          form.current,
+          'k17CIQh_qS8gMklCG',
+        )
+        .then(
+          result => {
+            console.log(result.text);
+            alert('Email enviado correctamente!!!');
+          },
+          error => {
+            console.log(error.text);
+            alert('Error al enviar el email!!!');
+          },
+        );
+    }
   };
 
   return (
@@ -68,6 +73,18 @@ const Contacto = () => {
           />
           <input className="submit" type="submit" value="Enviar" />
         </form>
+        <h2>O</h2>
+        <div className="message">
+          <h3 className="contact-card-title">Mandame un mensaje: </h3>
+          <a
+            href="https://api.whatsapp.com/send?phone=50685067325&text=Buenas, para poder contactarme con usted!"
+            className="contact-button"
+            target="_blank"
+          >
+            <i className="bx bxl-whatsapp"></i>
+            <span className="contact-card-data">(506) 8506-7325</span>
+          </a>
+        </div>
       </div>
     </div>
   );
